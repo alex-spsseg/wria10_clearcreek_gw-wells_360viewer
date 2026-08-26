2bd0ac03add1bc5d392e88e01960746d17f92b4e
alex-spsseg <alexa@spsseg.org>
2026-08-26 15:47:44 -0700

    Update for side by side test

---
+++ side-by-side/data.js
@@ -0,0 +1,73 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-old",
+      "name": "old",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 1.65215050133318,
+        "pitch": 0.72911161946619,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-new",
+      "name": "new",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2000,
+      "initialViewParameters": {
+        "yaw": 0.7424016716032984,
+        "pitch": 0.4712378875711778,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [],
+      "infoHotspots": []
+    }
+  ],
+  "name": "side by side test",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
